57bc3a2fdc3ae7acee014c0aee394e385998eba8
Anders Retzner <anders.retzner@gmail.com>
2023-11-17 08:46:11 +0100

    Update configuration and add API tests

--- app/conf/config.py
+++ app/conf/config.py
@@ -2,19 +2,27 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from functools import lru_cache
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file="../.env")
+    model_config = SettingsConfigDict(env_file=".env")
     environment: str
 
 class DatabaseSettings(BaseSettings):
-    model_config = SettingsConfigDict(env_file="../.env", env_prefix="DB_")
+    model_config = SettingsConfigDict(env_file=".env.prod", env_prefix="DB_")
+    dsn: str
+    database: str
+    collection: str
+
+class DevelopmentDatabaseSettings(DatabaseSettings):
+    model_config = SettingsConfigDict(env_file=".env.dev", env_prefix="DB_")
     dsn: str
     database: str
     collection: str
 
 @lru_cache(maxsize=None)
 def get_database_settings():
-    return DatabaseSettings()
-
-@lru_cache(maxsize=None)
-def get_settings():
-    return Settings()
+    settings = Settings()
+    if settings.environment == "development":
+        return DevelopmentDatabaseSettings()
+    elif settings.environment == "production":
+        return DatabaseSettings()
+    else:
+        raise ValueError(f"Invalid environment name ('{settings.environment}').")
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,58 @@
 from fastapi.testclient import TestClient
 from app.main import app
+import pytest
 
-client = TestClient(app)
+@pytest.fixture(scope="module")
+def client():
+    with TestClient(app) as c:
+        yield c
 
-def test_docs():
+def test_docs(client):
     response = client.get("/docs")
     assert response.status_code == 200
     assert response.headers["content-type"] == "text/html; charset=utf-8"
+
+def test_redoc(client):
+    response = client.get("/redoc")
+    assert response.status_code == 200
+    assert response.headers["content-type"] == "text/html; charset=utf-8"
+
+def test_get_daily_steps(client):
+    response = client.get("/api/v1/steps/")
+    assert response.status_code == 200
+    assert response.headers["content-type"] == "application/json"
+    assert type(response.json()) == list
+
+def test_get_daily_steps_by_date(client):
+    response = client.get("/api/v1/steps/2023-11-01")
+    assert response.status_code == 200
+    assert response.headers["content-type"] == "application/json"
+    assert type(response.json()) == dict
+    assert "ts" in response.json()
+    assert "steps" in response.json()
+
+def test_get_daily_steps_by_date_not_found(client):
+    response = client.get("/api/v1/steps/1961-01-01")
+    assert response.status_code == 404
+    assert response.headers["content-type"] == "application/json"
+    assert type(response.json()) == dict
+    assert "detail" in response.json()
+    assert response.json()["detail"] == "No daily steps found for 1961-01-01"
+
+def test_get_daily_steps_by_date_invalid_date(client):
+    response = client.get("/api/v1/steps/12345")
+    assert response.status_code == 422
+    assert response.headers["content-type"] == "application/json"
+    assert type(response.json()) == dict
+    assert "detail" in response.json()
+    assert response.json()["detail"][0]["msg"] == "Datetimes provided to dates should have zero time - e.g. be exact dates"
+
+def test_get_daily_steps_by_range(client):
+    response = client.get("/api/v1/steps/range/?start=2023-11-01&end=2023-11-10")
+    assert response.status_code == 200
+    assert response.headers["content-type"] == "application/json"
+    assert type(response.json()) == list
+    assert len(response.json()) == 10
+    for item in response.json():
+        assert "ts" in item
+        assert "steps" in item
